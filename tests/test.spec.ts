eba474410d58b951c4ba8535d081c6805692bc7e
fjx79nphrj-boop <fjx79nphrj@privaterelay.appleid.com>
2026-02-14 13:22:29 +0100

    Delete tests directory

--- tests/test.spec.ts
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('basic test', async ({ page }) => {
-  await page.goto('https://stepstrong.netlify.app/');
-  await expect(page).toHaveURL('https://stepstrong.netlify.app/**');
-});
