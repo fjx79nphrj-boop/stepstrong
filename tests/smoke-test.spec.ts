7a5b040031d8604cc6089ebcea8b6db40e37b561
fjx79nphrj-boop <fjx79nphrj@privaterelay.appleid.com>
2026-02-14 12:17:37 +0100

    Update expected title in smoke test

--- tests/smoke-test.spec.ts
+++ tests/smoke-test.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('My PWA loads', async ({ page }) => {
   await page.goto('/');
-  await expect(page).toHaveTitle(/.*PWA Title Here/);  // Change to your app's title
+  await expect(page).toHaveTitle(/.Steadfast/);  // Change to your app's title
 });
